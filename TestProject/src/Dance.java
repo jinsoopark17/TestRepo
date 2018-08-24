c241a79b4c45c8ede55813ad0f9aca85ceefa4c6
jinsoopark <jinsoopark17@augustana.edu>
2018-08-24 12:40:50 -0500

    "Added dancing and saying goodbye."

--- TestProject/src/Dance.java
+++ TestProject/src/Dance.java
@@ -1,9 +1,16 @@
-
+/*
+ * Author : Jinsoo Park
+ * Created : Aug.24.2018
+ * LastModified : Aug.24.2018
+ * Discription : Lab 01 for CSC 285
+ *
+ */
 public class Dance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Watch those chickens dance!");
+		
 	}
 
 }
