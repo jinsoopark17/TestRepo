5f4418043ea23d842876da4fe36b992ea262df1e
jinsoopark <jinsoopark17@augustana.edu>
2018-08-24 12:32:53 -0500

    Added the new Eclipse project to the repo

---
+++ TestProject/src/TestMain.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ * Author : Jinsoo Park
+ * Created : Aug.24.2018
+ * Last Modified : Aug.24.2018
+ * 
+ * Description: Lab 01 for CSC 285
+ * 
+ */
+
+
+public class TestMain {
+	
+	public static void main (String[]args) {
+		
+		System.out.println("Hello Chicken World!");
+	}
+	
+}
